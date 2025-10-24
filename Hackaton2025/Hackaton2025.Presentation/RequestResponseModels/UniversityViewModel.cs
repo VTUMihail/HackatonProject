d011dd938ad17bd7a4a26372335c718a7a36c274
Bozhidar <mrarthur0507@gmail.com>
2025-10-24 11:10:01 +0300

    universities controller

---
+++ Hackaton2025/Hackaton2025.Presentation/RequestResponseModels/UniversityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Hackaton2025.Domain.Models.Entities;
+
+namespace Hackaton2025.Presentation.RequestResponseModels
+{
+    public class UniversityViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<UniversityFaculty> Faculties { get; set; }
+    }
+}
